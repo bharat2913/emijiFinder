c5819554eaa198cc32aeffa7b653b8899b4b7b79
bharat2913 <bs987143@gmail.com>
2021-03-23 04:43:15 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,29 @@ var emojiDictionary = {
   "😔": "sad",
   "🥡": "takeout box",
   "❤️": "love",
-  "😑": "annoyance"
+  "😑": "annoyance",
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "😇": "Smiling Face with Halo",
+  "🥰": "Smiling Face with Hearts",
+  "😍": "Smiling Face with Heart-Eyes",
+  "🤩": "Star-Struck",
+  "😘": "Face Blowing a Kiss",
+  "😗": "Kissing Face",
+  "☺️": "Smiling Face",
+  "😚": "Kissing Face with Closed Eyes",
+  "😙": "Kissing Face with Smiling Eyes"
 };
+var emojiweknow = Object.keys(emojiDictionary);
 
 export default function App() {
   const [meaning, setMeaningInput] = useState("");
@@ -39,10 +60,33 @@ export default function App() {
   // function listClickItenHandler(item) {
   //   console.log("Clicked", item);
   // }
+  function emojiClickHandler(emoji) {
+    // processing
+    var meaning = emojiDictionary[emoji];
+    setMeaningInput(meaning); // react call to show output
+  }
 
   return (
     <div className="App">
-      <input onChange={emojiInputHandler}></input>
+      <h1>Emoji Detect</h1>
+
+      {emojiweknow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+
+      <input
+        placeholder="Click or Enter one of these emojis to get meaning "
+        onChange={emojiInputHandler}
+      ></input>
+
       <div> {meaning}</div>
 
       {/* <ul>
